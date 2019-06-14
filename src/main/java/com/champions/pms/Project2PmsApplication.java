7bdb9b7bebca76a7114e0642562081063d6c2d83
Mule Sknr <soymilk@airmail.cc>
2019-06-14 09:54:25 -0400

    Initial spring project

---
+++ src/main/java/com/champions/pms/Project2PmsApplication.java
@@ -0,0 +1,13 @@
+package com.champions.pms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project2PmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project2PmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/champions/pms/Project2PmsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.champions.pms;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Project2PmsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
