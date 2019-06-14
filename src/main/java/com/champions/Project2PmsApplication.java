47c26ad9fe0baaaf96a3f2ea8f68b0fc1b5e9fd1
Mule Sknr <soymilk@airmail.cc>
2019-06-14 11:14:03 -0400

    webconfig and depth filter

--- src/main/java/com/champions/Project2PmsApplication.java
+++ src/main/java/com/champions/Project2PmsApplication.java
@@ -1,9 +1,12 @@
-package com.champions.pms;
+package com.champions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class Project2PmsApplication {
 
 	public static void main(String[] args) {
