c4bdc0ef9e9bc34f0851046c4cd4d56c8dbd3383
Mule Sknr <soymilk@airmail.cc>
2019-06-19 10:11:05 -0400

    Working partial update PATCH endpoint for user

--- src/main/java/com/champions/services/UserServiceImpl.java
+++ src/main/java/com/champions/services/UserServiceImpl.java
@@ -8,18 +8,25 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.champions.models.Permit;
+import com.champions.models.Role;
 import com.champions.models.User;
+import com.champions.repositories.RoleDao;
 import com.champions.repositories.UserDao;
 
 @Service
 public class UserServiceImpl implements UserService {
 
 	private UserDao userDao;
+	private RoleService roleService;
+	private PermitService permitService;
 	
 	@Autowired
-	public UserServiceImpl(UserDao ud) {
+	public UserServiceImpl(UserDao ud, RoleService rs, PermitService ps) {
 		
 		this.userDao = ud;
+		this.roleService = rs;
+		this.permitService = ps;
 	}
 	
 	@Override
@@ -43,22 +50,70 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public User update(Map<String, Object> updates, int id) {
 		
-		/**
+		// test output
+		updates.forEach((s, o) -> {System.out.println(s + " " + o + " " + o.getClass());});
+		
 		User user = null;
-		Optional<User> result = userDao.findById(id);
 		
 		try {
-			
-			user = result.get();
+			user = userDao.findById(id).get();
 			
 		} catch(NoSuchElementException e) {
 			
-			System.out.println("Update User failed to find user of id" + id);
+			System.out.println("UserService update method failed to find user of ID " + id);
 		}
-		*/
-		updates.forEach((s, o) -> {System.out.println(s + " " + o);});
 		
-		return null;
+		if(user != null) {
+			
+			final User finalUser = user;
+			updates.forEach((s, o) -> {fillUser(finalUser, s, o);});
+			save(user);
+		}
+		
+		return user;
+	}
+	
+	private void fillUser(User usr, String s, Object o) {
+		
+		switch(s) {
+		
+			case "username":
+				usr.setUsername((String)o);
+				break;
+				
+			case "password":
+				usr.setPassword((String)o);
+				break;
+				
+			case "address":
+				usr.setAddress((String)o);
+				break;
+				
+			case "email":
+				usr.setEmail((String)o);
+				break;
+				
+			case "firstName":
+				usr.setFirstName((String)o);
+				break;
+				
+			case "lastName":
+				usr.setLastName((String)o);
+				break;
+				
+			case "roleId":
+				Role role = roleService.findById((Integer)o);
+				if(role != null) { usr.setRole(role); }
+				break;
+				
+			case "permitId":
+				Permit permit = permitService.findById((Integer)o);
+				if(permit != null) { usr.setPermit(permit); }
+				break;
+				
+			default:
+				break;
+		}
 	}
 
 }
