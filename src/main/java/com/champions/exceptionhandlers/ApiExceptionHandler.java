a639cfa54048fd7e01d761ac79ca111271e43c42
Mule Sknr <soymilk@airmail.cc>
2019-06-20 16:44:18 -0400

    Added additional exception types to handler

--- src/main/java/com/champions/exceptionhandlers/ApiExceptionHandler.java
+++ src/main/java/com/champions/exceptionhandlers/ApiExceptionHandler.java
@@ -5,24 +5,38 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.champions.exceptions.PermitNotFoundException;
+import com.champions.exceptions.RoleNotFoundException;
 import com.champions.exceptions.UserNotFoundException;
 
 @ControllerAdvice
 public class ApiExceptionHandler {
 	
 	@ExceptionHandler(value = {UserNotFoundException.class})
-	public ResponseEntity<Object> userIdNotFound (UserNotFoundException e) {
+	public ResponseEntity<Object> userNotFound(UserNotFoundException e) {
 		
 		System.out.println(e);
+		return new ResponseEntity<Object>(e.getMessage(), e.getStatus());
+	}
+	
+	@ExceptionHandler(value = {PermitNotFoundException.class})
+	public ResponseEntity<Object> permitNotFound(PermitNotFoundException e) {
 		
-		return new ResponseEntity<Object> (e.getMessage(), e.getStatus());
+		System.out.println(e);
+		return new ResponseEntity<Object>(e.getMessage(), e.getStatus());
+	}
+	
+	@ExceptionHandler(value = {RoleNotFoundException.class})
+	public ResponseEntity<Object> roleNotFound(RoleNotFoundException e) {
+		
+		System.out.println(e);
+		return new ResponseEntity<Object>(e.getMessage(), e.getStatus());
 	}
 	
 	@ExceptionHandler(value = {Throwable.class}) 
 	public ResponseEntity<Object> catchAll(Throwable e) {
 		
 		System.out.println(e);
-		
 		return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 }
